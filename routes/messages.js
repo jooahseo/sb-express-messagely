95398ef14a7241ea101434bb5193c2b610608979
Jooah Seo <seo.jooah@gmail.com>
2021-08-11 21:48:14 -0400

    add router methods

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { Message } = require("../models/message");
+const ExpressError = require("../expressError");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,7 +16,20 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
-
+router.get("/:id", (req, res, next) => {
+  try {
+    const m = Message.get(req.params.id);
+    if (
+      req.user.username === m.from_user.username ||
+      req.user.username === m.to_user.username
+    ) {
+      return res.json({ message: m });
+    }
+    throw new ExpressError("Unauthorized", 401);
+  } catch (e) {
+    return next(e);
+  }
+});
 
 /** POST / - post message.
  *
@@ -18,8 +37,20 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
-
-
+router.post("/", ensureLoggedIn, (req, res, next) => {
+  try {
+    const { to_username, body } = req.body;
+    const from_username = req.user.username;
+    if (!to_username || !body) {
+      throw new ExpressError("to_username and body are required", 400);
+    }
+    const messageObj = { from_username, to_username, body };
+    const m = Message.create(messageObj);
+    return res.json({ message: m });
+  } catch (e) {
+    return next(e);
+  }
+});
 /** POST/:id/read - mark message as read:
  *
  *  => {message: {id, read_at}}
@@ -27,4 +58,16 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
-
+router.post("/:id/read", (req, res, next) => {
+  try {
+    const m = Message.get(req.params.id);
+    if (m.to_user.username !== req.user.username) {
+      throw new ExpressError("Unauthorized", 401);
+    }
+    const read = Message.markRead(req.params.id);
+    return res.json({ message: read });
+  } catch (e) {
+    return next(e);
+  }
+});
+module.exports = router;
